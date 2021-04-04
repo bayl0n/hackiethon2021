fec7baecd8c414757cf6b487c203df657a16becb
surga <surga3.carlos@gmail.com>
2021-04-04 22:23:38 +1000

    styling

--- frontend/src/pages/Navigation/styles.js
+++ frontend/src/pages/Navigation/styles.js
@@ -3,9 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   navBar: {
     color: 'white',
-    backgroundcolor: 'white',
+    backgroundcolor: 'primary',
   },
   link: {
     marginleft: '20px',
   },
+
 }));
--- frontend/src/pages/Pomodoro/Pomodoro.js
+++ frontend/src/pages/Pomodoro/Pomodoro.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Container, Typography, Grid } from '@material-ui/core';
+import useStyles from '../Pomodoro/style';
+import Navigation from '../Navigation/Navigation'
 import Timer from '../../components/Timer/Timer';
 
 const Pomodoro = () => {
+    const classes = useStyles();
 
     const [isPlaying, setIsPlaying] = useState(false);
 
@@ -11,12 +14,15 @@ const Pomodoro = () => {
     }
 
     return (
-        <Grid container align="center" justify="center" direction="column">
+        <React.Fragment>
+            <Navigation/>
+        <Grid container align="center" justify= "center" direction= "column"  className={classes.body}>
             <Typography variant="h1" color="primary">Pomodoro Timer</Typography>
-            <Container>
+            <Container >
                 <Timer/>
             </Container>
         </Grid>
+        </React.Fragment>
     );
 };
 
